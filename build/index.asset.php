578b39c15192d92c109ba863b02f7de2c2b677b6
Tewodros Abebe <teddyab729@gmail.com>
2023-07-29 19:21:04 +0300

    main

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'e909b376be1ca7d5aa0e');
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import App from "./App";
-import { render } from "@wordpress/element";
-import "./index.css";
-
-
-// Render the App component into the DOM
-render(<App />, document.getElementById("samanta-content-evaluator"));
